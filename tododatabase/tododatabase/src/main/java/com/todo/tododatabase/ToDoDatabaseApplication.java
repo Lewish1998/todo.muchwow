b9b45ed08c904564f88040102b0a615fe5e3dc8d
Lewis <lewishalstead5@gmail.com>
2023-10-12 17:34:39 +0100

    SB backend

---
+++ tododatabase/tododatabase/src/main/java/com/todo/tododatabase/ToDoDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.todo.tododatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoDatabaseApplication.class, args);
+	}
+
+}
