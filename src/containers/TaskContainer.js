5ae6cbd673d2bc800a3351c5ca8f4691f7d42c52
Lewis <lewishalstead5@gmail.com>
2023-10-15 16:32:20 +0100

    redirect proxy issue

--- src/containers/TaskContainer.js
+++ src/containers/TaskContainer.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import ToDoList from '../components/ToDoList'
+import Request from './helpers/Request'
 
 const TaskContainer = () => {
+
+    const [tasks, setTasks] = useState([])
+
+    function componentDidMount() {
+        const request = new Request();
+        request.get('api/tasks')
+        .then((data) => {console.log(data)})
+    }
+
+    componentDidMount()
+
   return (
     <>
-        Task Container
+
     </>
   )
 }
